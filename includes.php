c66f70de31145d4a9e37bc2ce82a5d549b8a47c6
masimansari <sd@aitchison.edu.pk>
2018-10-16 10:20:29 +0500

    acreports-001

---
+++ includes.php
@@ -0,0 +1,31 @@
+
+	<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title><?php echo $title; ?> | Aitchison </title>
+    	
+        <link rel="icon" type="image/x-icon" href="<?php echo $path; ?>favicon.ico" />
+        
+        <link rel="shortcut icon" href="<?php echo $path; ?>favicon.ico" type="image/x-icon">
+        
+        <!-- Bootstrap -->
+        <link href="<?php echo $path; ?>css/bootstrap.min.css" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="<?php echo $path; ?>css/font-awesome.min.css" rel="stylesheet">
+        
+        <!-- Custom Style -->
+        <link href="<?php echo $path; ?>css/style.css" rel="stylesheet">
+        <link href="<?php echo $path; ?>classes/controls/datepicker/css/bootstrap-datepicker.standalone.css" rel="stylesheet">
+<style>
+
+.table thead tr th {
+	background:#204198;
+	color:#fff;
+}
+</style>
+        
+	</head>
